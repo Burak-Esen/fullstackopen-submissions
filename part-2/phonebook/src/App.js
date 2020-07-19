76bcfd87cb05b13eb570ef54c07c4a4656ca6bbc
Burak-Esen <0.burak.esen@gmail.com>
2020-07-20 01:26:36 +0300

    phonebook 2.11

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PersonForm from './Components/PersonForm'
 import Filter from "./Components/Filter"
 import Persons from "./Components/Persons"
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filt , setFilt ] = useState('')
-  
 
+  useEffect(()=>{
+    async function Respond(){
+      const promise = await axios.get("http://localhost:3001/persons")
+      setPersons(promise.data)
+    }
+    Respond().catch(e => {
+      console.log('There has been a problem with your fetch operation: ' + e.message);
+    });
+  },[])
+  
   
   return (
     <div>
