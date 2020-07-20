34079ac906c5764523e877e3a6a2f440f200cf50
Burak-Esen <0.burak.esen@gmail.com>
2020-07-20 15:20:23 +0300

    dataforcountries 2.13

--- part-2/dataforcountries/src/components/Countries.js
+++ part-2/dataforcountries/src/components/Countries.js
@@ -1,24 +1,14 @@
 import React from 'react'
+import FilteredCountries from './FilteredCountries'
 
 const Countries = (props) => {
   let countryNamesWithFilter =props.data.filter(country=>country.name.toLowerCase().includes(props.filterPhrase))
-  let resultOfNames = countryNamesWithFilter.length>10 ? "Too many matches, specify another filter" 
-    : countryNamesWithFilter.map((country, i)=>{
-      return <div key={i}>
-        <h2>{i+1}. {country.name}</h2>
-        <p>Capital: {country.capital}</p>
-        <p>Population: {country.population}</p>
-        <br/>
-        <h3 style={{marginLeft:7}}>languages</h3>
-        <ul>
-          {country.languages.map(lang=><li key={lang.name}>{lang.name}</li>)}
-        </ul>
-        <img height={150} src={country.flag} alt={country.name + ' flag'}/>
-      </div>
-  })
+  let resultOfSpecificFilter = countryNamesWithFilter.length>10 ? false : countryNamesWithFilter
+  
   return (
     <div>
-      {resultOfNames}
+      {resultOfSpecificFilter ? <FilteredCountries countries={resultOfSpecificFilter} /> 
+        : "Too many matches, specify another filter"}
     </div>
   )
 }
---
+++ part-2/dataforcountries/src/components/FilteredCountries.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+const FilteredCountries = (props) => {
+  let [showFilter, setShowFilter ] = useState(new Array(props.countries.length).fill(false))
+  const showHandler = e =>{
+    let index=e.target.getAttribute('index')
+    let temp=[].concat(showFilter)
+    if(temp[index]===false){
+      temp[index]=true
+    }else{
+      temp[index]=false
+    }
+    setShowFilter(temp)
+  }
+  return (
+    <div>
+      {props.countries.map((country, i)=><div key={i}>
+          <h2 style={{display:'inline'}}>{i+1}. {country.name}</h2>
+          <button style={{display:'inline-block'}} onClick={showHandler} index={Number(i)}>show</button>
+          <div style={{display:showFilter[i] ? "initial" : "none"}}>
+            <p>Capital: {country.capital}</p>
+            <p>Population: {country.population}</p>
+            <br/>
+            <h3 style={{marginLeft:7}}>languages</h3>
+            <ul>
+              {country.languages.map(lang=><li key={lang.name}>{lang.name}</li>)}
+            </ul>
+            <img height={150} src={country.flag} alt={country.name + ' flag'}/>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default FilteredCountries
