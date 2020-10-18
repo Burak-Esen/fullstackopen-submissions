9b4cb5cb5dcc5a1cf8caf82f9cc1757727b096f1
Burak-Esen <0.burak.esen@gmail.com>
2020-10-18 21:10:27 +0200

    bloglist frontend test 5.14

--- part-5/bloglist-frontend/src/test/Blog.test.js
+++ part-5/bloglist-frontend/src/test/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
 test('renders content', () => {
@@ -20,4 +20,21 @@ test('renders content', () => {
   expect(component.container).not.toHaveTextContent('likes')
   expect(component.container).not.toHaveTextContent('Url')
   expect(component.container).not.toHaveTextContent('Category')
+})
+
+test('show details when cilick details', () => {
+  const blog = {
+    previewUrl:'https://static.npmjs.com/338e4905a2684ca96e08c7780fc68412.png',
+    likes:9,
+    title:'link-preview-generator',
+    author:'Andrej Gajdos/npm',
+    url:'https://www.npmjs.com/package/link-preview-generator'
+  }
+
+  const mockHandler = jest.fn()
+  const component = render(<Blog blog={blog} getTokenFromWindow={mockHandler} setBlogs={mockHandler} />)
+  fireEvent.click(component.container.querySelector('h3'))
+
+  expect(component.container).toHaveTextContent('likes')
+  expect(component.container).toHaveTextContent('Url')
 })
