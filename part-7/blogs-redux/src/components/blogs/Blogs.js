306e2e1f298ab5f1f7e6c58dd9a888116be5ba15
Burak-Esen <0.burak.esen@gmail.com>
2020-12-06 14:52:24 +0100

    blogs-redux 7.13 7.15 remake

--- part-7/blogs-redux/src/components/blogs/Blogs.js
+++ part-7/blogs-redux/src/components/blogs/Blogs.js
@@ -5,12 +5,20 @@ import { Link } from 'react-router-dom'
 
 const Blogs = () => {
   const blogs = useSelector(state=>state.blogs)
+  const style = {
+    'border':'1px solid black',
+    'border-radius':'0.5rem',
+    'paddingLeft':'0.5rem',
+    'marginBottom':'0.5rem',
+    'listStyle':'none',
+    'marginLeft':'-2rem'
+  }
   return (
     <div>
       <CreateBlog />
       <ul>
         {blogs.map(blog => 
-          <li key={blog.id}>
+          <li style={style}key={blog.id}>
             <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
           </li>
         )}
--- part-7/blogs-redux/src/routes.js
+++ part-7/blogs-redux/src/routes.js
@@ -2,7 +2,9 @@ import React, {useEffect} from 'react'
 import { Route } from 'react-router-dom'
 import Main from './components/Main'
 import User from './components/users/User'
+import Users from './components/users/Users'
 import Blog from './components/blogs/Blog'
+import Blogs from './components/blogs/Blogs'
 import { useSelector, useDispatch } from 'react-redux'
 import { getAllUsers } from './reducers/userReducer'
 import { getAllBlogs } from './reducers/blogReducer'
@@ -19,6 +21,9 @@ const BaseRouter = () => {
   return (
     <div>
       <Route exact path="/" component={Main} />
+      <Route exact path="/users" component={Users} />
+      <Route exact path="/blogs" component={Blogs} />
+
       {userIds.map(id=><Route exact path={`/users/${id}`} component={User}  key={id}/>)}
       {blogIds.map(id=><Route exact path={`/blogs/${id}`} component={Blog}  key={id}/>)}
     </div>
