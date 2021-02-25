425308fb882e806ff8de506be68aa654c8c052e5
Burak-Esen <0.burak.esen@gmail.com>
2021-02-25 20:00:50 +0100

    WebBMI 9.5

--- part-9/bmi/bmiCalculator.ts
+++ part-9/bmi/bmiCalculator.ts
@@ -1,16 +1,3 @@
-/*
-Category	BMI (kg/m2)
-                                        from	  to
-Very severely underweight                 -		  15
-Severely underweight                    	15	  16
-Underweight                             	16	  18.5
-Normal (healthy weight)                 	18.5  25
-Overweight	                              25	  30
-Obese Class I (Moderately obese)        	30	  35
-Obese Class II (Severely obese)	          35	  40
-Obese Class III (Very severely obese)   	40    -
-*/
-
 const parseArguments = (args: Array<string>) => {
   if (args.length < 4) throw new Error('Not enough arguments');
   if (args.length > 4) throw new Error('Too many arguments');
@@ -25,7 +12,7 @@ const parseArguments = (args: Array<string>) => {
   }
 }
 
-const calculateBmi = (height:number, weight:number):string => {
+export const calculateBmi = (height:number, weight:number):string => {
   const index = weight/(height/100)**2
   let result = ''
   if (index<15) {
--- part-9/bmi/index.ts
+++ part-9/bmi/index.ts
@@ -1,11 +1,26 @@
 import express from 'express'
+import { calculateBmi } from './bmiCalculator'
 const app = express()
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
 })
 
-const PORT = 3003
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+  if (isNaN(height) || isNaN(weight)) {
+    return res.json({ error: 'malformatted parameters' })
+  }
+  const bmiRes = {
+    weight,
+    height,
+    bmi:calculateBmi(height, weight)
+  }
+  return res.json(bmiRes)
+})
+
+const PORT = 3002
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
