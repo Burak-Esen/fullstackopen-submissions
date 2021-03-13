7abea69eeb5bd20375a28e94aaf968809c19b4c8
Burak-Esen <0.burak.esen@gmail.com>
2021-03-13 23:35:45 +0100

    exercise 9.15

--- part-9/courses/src/index.tsx
+++ part-9/courses/src/index.tsx
@@ -8,49 +8,136 @@ interface Course {
   name:string
 }
 
-interface CoursePart {
-  name:string
-  exerciseCount:number
-}
-
 interface CourseList {
   courseList:CoursePart[]
 }
 
 const Header:React.FC<Course> = (props) => {
   return <h1>{props.name}</h1>;
-}
+};
+
+const Part: React.FC<{part:CoursePart}> = (props) => {
+  //↑ type {part:CoursePart} or CoursePart directly
+  //↑ when use second one: props is directly CoursePart object
+  //↑ so component <Part {...part} key={i} />
+  /**
+  * Helper function for exhaustive type checking
+  */
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+  switch (props.part.name) {
+    case 'Fundamentals':
+      return <div>
+        <h3>{props.part.name}</h3>
+        <p>description: {props.part.description}</p>
+        <p>exercise count: {props.part.exerciseCount}</p>
+      </div>
+    case 'Using props to pass data':
+      return <div>
+        <h3>{props.part.name}</h3>
+        <p>group project count: {props.part.groupProjectCount}</p>
+        <p>exercise count: {props.part.exerciseCount}</p>
+      </div>
+    case 'Deeper type usage':
+      return <div>
+        <h3>{props.part.name}</h3>
+        <p>description: {props.part.description}</p>
+        <p>submission link: {props.part.exerciseSubmissionLink}</p>
+        <p>exercise count: {props.part.exerciseCount}</p>
+      </div>
+    case 'Backend development':
+      return <div>
+      <h3>{props.part.name}</h3>
+        <p>description: {props.part.description}</p>
+        <p>exercise count: {props.part.exerciseCount}</p>
+      </div>
+    default:
+      return assertNever(props.part)
+  }
+};
 
 const Content: React.FC<CourseList> = (props) => {
   return <div>
     {
       props.courseList.map((part, i) => (
-        <p key={i}>{part.name} {part.exerciseCount}</p>
+        <Part part={part} key={i} />
       ))
     }
   </div>
-}
+};
 
 const Total: React.FC<CourseList> = (props) => {
   return <p>Number of exercises{' '}
   {props.courseList.reduce((a, b) => a + b.exerciseCount, 0)}
   </p>
+};
+
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
 }
+interface BaseCoursePartWithDesc extends CoursePartBase {
+  description: string;
+  groupProjectCount? : number;
+  exerciseSubmissionLink?: string;
+}
+
+
+interface CoursePartOne extends BaseCoursePartWithDesc {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends BaseCoursePartWithDesc {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends BaseCoursePartWithDesc {
+  name: "Backend development";
+  requirements?: string[];
+  type?:string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour ;
+
+
+
+
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
   ];
 
